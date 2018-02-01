6f80231b723a41f6bdfcc8c245552626c50c2852
shx <shx0518@gmail.com>
2018-02-01 16:59:58 +0800

    首页

--- app/src/main/java/com/shx/lawwh/activity/MainActivity.java
+++ app/src/main/java/com/shx/lawwh/activity/MainActivity.java
@@ -1,96 +1,119 @@
 package com.shx.lawwh.activity;
 
-import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
 import com.shx.lawwh.R;
-import com.shx.lawwh.adapter.LoopViewPagerAdapter;
 import com.shx.lawwh.base.BaseActivity;
-import com.shx.lawwh.base.LayoutValue;
-import com.shx.lawwh.base.ViewPagerScheduler;
-import com.shx.lawwh.libs.dialog.ToastUtil;
-import com.shx.lawwh.view.ViewPageWithIndicator;
+import com.shx.lawwh.fragment.MainFragment;
+
 
 /**
- * Created by xuan on 2017/12/24.
+ * 首页
  */
+public class MainActivity extends BaseActivity implements View.OnClickListener {
+    private FrameLayout mContent;
+    private Fragment mMainFragment, mPersonCenterFragment, mFavoriteFragment;
+    private TextView mMain, mFavorite, mAccountCenter;
+    private FragmentManager mFragmentManager;
 
-public class MainActivity extends BaseActivity implements View.OnClickListener{
-    private ViewPageWithIndicator mLoopView;
-    private ImageView[] imageViews;
-    private LoopViewPagerAdapter loopViewPagerAdapter;
-    private ViewPagerScheduler vps;
-    private LinearLayout mFgbzk,mAjzjk,mWxhxp,mFhjj;
-    private int res[] = new int[]{R.drawable.img_banner1,R.drawable.img_banner2,R.drawable.img_banner3};
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         initView();
+        mFragmentManager = getSupportFragmentManager();
+        mMain.performClick();
     }
-    private void initView(){
-        mFgbzk= (LinearLayout) findViewById(R.id.layout_fgbz);
-        mAjzjk= (LinearLayout) findViewById(R.id.layout_ajzj);
-        mWxhxp= (LinearLayout) findViewById(R.id.layout_wxhxp);
-        mFhjj= (LinearLayout) findViewById(R.id.layout_fhjj);
-        mFhjj.setOnClickListener(this);
-        mFgbzk.setOnClickListener(this);
-        mWxhxp.setOnClickListener(this);
-        mAjzjk.setOnClickListener(this);
-        mFhjj.setOnClickListener(this);
-        mLoopView = (ViewPageWithIndicator) findViewById(R.id.vp_viewpage);
-        mLoopView.setFocusable(true);
-        mLoopView.setFocusableInTouchMode(true);
-        mLoopView.requestFocus();
-        initBanner();
-    }
-    /**
-     * 初始化首页Banner
-     */
-    private void initBanner() {
-        mLoopView.getLayoutParams().height = LayoutValue.SCREEN_WIDTH * 300 / 640;
-        mLoopView.setFocusable(true);
-        mLoopView.setFocusableInTouchMode(true);
-        mLoopView.requestFocus();
-        imageViews = new ImageView[res.length];
-        //循环创建ImageView，并且用Glide讲图片显示在上面
-        for (int i = 0; i < res.length; i++) {
-            ImageView imageView = new ImageView(this);
-            imageViews[i] = imageView;
-            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
-            Glide.with(this).load(res[i]).placeholder(getResources().getDrawable(R.drawable.img_banner)).into(imageView);
-        }
 
-        loopViewPagerAdapter = new LoopViewPagerAdapter(
-                imageViews);
-        vps = new ViewPagerScheduler(mLoopView.getViewPager());
-        loopViewPagerAdapter.setVps(vps);
-        vps.updateCount(imageViews.length);
-        vps.restart(4000);
-        mLoopView.setIndicatorVisibility(true, 5, 1);
-        mLoopView.setAdapter(loopViewPagerAdapter, imageViews.length);
+
+    @Override
+    public void onBackPressed() {
+        //连续按两次back键退出APP
+        dealAppBack();
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    private void initView() {
+        mMain = (TextView) findViewById(R.id.rb_main);
+        mFavorite = (TextView) findViewById(R.id.rb_favorite);
+        mAccountCenter = (TextView) findViewById(R.id.rb_my);
+        mContent = (FrameLayout) findViewById(R.id.content);
+        mMain.setOnClickListener(this);
+        mFavorite.setOnClickListener(this);
+        mAccountCenter.setOnClickListener(this);
+        setSelected();
+    }
+
+    /**
+     * 根据传入的index参数来设置选中的tab页。
+     */
+
+
+    //重置所有文本的选中状态
+    private void setSelected() {
+        mMain.setSelected(false);
+        mFavorite.setSelected(false);
+        mAccountCenter.setSelected(false);
+    }
+
+
+    private void showMainFragment() {
+        mMainFragment = new MainFragment();
+        FragmentTransaction transaction = mFragmentManager.beginTransaction();
+        transaction.replace(R.id.content, mMainFragment);
+        transaction.commitAllowingStateLoss();
+
+    }
+
+    private void showfavoriteFragment() {
+//        mShoppingCartFragment = new ShoppingCartFragment();
+//        FragmentTransaction transaction = mFragmentManager.beginTransaction();
+//        transaction.replace(R.id.content, mShoppingCartFragment);
+//        transaction.commitAllowingStateLoss();
+
+    }
+
+    private void showUserFragment() {
+//        mPersonCenterFragment = new UserFragment();
+//        FragmentTransaction transaction = mFragmentManager.beginTransaction();
+//        transaction.replace(R.id.content, mPersonCenterFragment);
+//        transaction.commitAllowingStateLoss();
+
+    }
+
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.layout_fgbz:
-                startActivity(new Intent(this,LawActivity.class));
+        switch (v.getId()) {
+            case R.id.rb_favorite:
+                setSelected();
+                mFavorite.setSelected(true);
+                showfavoriteFragment();
                 break;
-            case R.id.layout_ajzj:
-                startActivity(new Intent(this,SSActivity.class));
+            case R.id.rb_main:
+                setSelected();
+                mMain.setSelected(true);
+                showMainFragment();
                 break;
-            case R.id.layout_wxhxp:
-                startActivity(new Intent(this,ChemicalsActivity.class));
-                break;
-            case R.id.layout_fhjj:
-                ToastUtil.getInstance().toastInCenter(this,"该功能暂未开放使用");
+
+            case R.id.rb_my:
+                setSelected();
+                mAccountCenter.setSelected(true);
+                showUserFragment();
                 break;
         }
+
     }
 }
