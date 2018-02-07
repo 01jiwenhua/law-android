5db0a7c0571be22109591c4a4d9a8684d527fd16
xuan <shx0518@gmail.com>
2018-02-08 00:29:28 +0800

    修改PDFview

--- app/src/main/java/com/shx/lawwh/activity/PdfViewActivity.java
+++ app/src/main/java/com/shx/lawwh/activity/PdfViewActivity.java
@@ -5,11 +5,13 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.github.barteksc.pdfviewer.listener.OnDrawListener;
 import com.github.barteksc.pdfviewer.listener.OnErrorListener;
 import com.github.barteksc.pdfviewer.listener.OnLoadCompleteListener;
 import com.github.barteksc.pdfviewer.listener.OnPageChangeListener;
+import com.github.barteksc.pdfviewer.listener.OnPageScrollListener;
 import com.shx.lawwh.R;
 import com.shx.lawwh.base.BaseActivity;
 import com.shx.lawwh.common.CommonValues;
@@ -24,6 +26,10 @@ import com.shx.lawwh.utils.SharedPreferencesUtil;
 import com.shx.lawwh.view.PDFView;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import static com.shx.lawwh.R.id.pdfView;
 
@@ -82,31 +88,98 @@ public class PdfViewActivity extends BaseActivity implements OnPageChangeListene
         int index = mUrl.lastIndexOf("/");
         String fileName = mUrl.substring(index);
         DialogManager.getInstance().showProgressDialogNotCancelbale(this);
-        mView.fromUrl(mUrl, fileName, new PDFView.FileLoadingListener() {
+        new Thread(new Runnable() {
             @Override
-            public void onFileLoaded(File file) {
-                isLoaded = true;
-                Log.d("PDFView", "PDF File 加载完成======");
-                mView.fromFile(file)
-                        .enableSwipe(true) // allows to block changing pages using swipe
-                        .defaultPage(0)
-                        .onError(PdfViewActivity.this)
-                        // allows to draw something on the current page, usually visible in the middle of the screen
-                        .onDraw(PdfViewActivity.this)
-                        // allows to draw something on all pages, separately for every page. Called only for visible pages
-                        .onLoad(PdfViewActivity.this) // called after document is loaded and starts to be rendered
-                        .onPageChange(PdfViewActivity.this)
-                        .swipeHorizontal(false)
-                        .enableAntialiasing(true)
-                        .load();
-            }
+            public void run() {
 
-            @Override
-            public void onFileLoadFail() {
-                isLoaded = false;
-                DialogManager.getInstance().dissMissProgressDialog();
+                try {
+                    URL url = new URL(mUrl);
+                    HttpURLConnection connection = (HttpURLConnection)
+                            url.openConnection();
+                    connection.setRequestMethod("GET");//试过POST 可能报错
+                    connection.setDoInput(true);
+                    connection.setConnectTimeout(10000);
+                    connection.setReadTimeout(10000);
+                    //实现连接
+                    connection.connect();
+
+                    System.out.println("connection.getResponseCode()=" + connection.getResponseCode());
+                    if (connection.getResponseCode() == 200) {
+                        InputStream is = connection.getInputStream();
+                        //这里给过去就行了
+                        mView.fromStream(is)
+//.pages(0, 2, 1, 3, 3, 3) // all pages are displayed by default
+                                .enableSwipe(true)
+                                .swipeHorizontal(false)
+                                .enableDoubletap(true)
+                                .defaultPage(0)
+                                .onDraw(new OnDrawListener() {
+                                    @Override
+                                    public void onLayerDrawn(Canvas canvas, float pageWidth, float pageHeight, int displayedPage) {
+
+                                    }
+                                })
+                                .onLoad(new OnLoadCompleteListener() {
+                                    @Override
+                                    public void loadComplete(int nbPages) {
+                                        DialogManager.getInstance().dissMissProgressDialog();
+                                        Toast.makeText(getApplicationContext(), "loadComplete", Toast.LENGTH_SHORT).show();
+                                    }
+                                })
+                                .onPageChange(new OnPageChangeListener() {
+                                    @Override
+                                    public void onPageChanged(int page, int pageCount) {
+
+                                    }
+                                })
+                                .onPageScroll(new OnPageScrollListener() {
+                                    @Override
+                                    public void onPageScrolled(int page, float positionOffset) {
+
+                                    }
+                                })
+                                .onError(new OnErrorListener() {
+                                    @Override
+                                    public void onError(Throwable t) {
+                                        Toast.makeText(getApplicationContext(), "error", Toast.LENGTH_SHORT).show();
+                                    }
+                                })
+                                .enableAnnotationRendering(false)
+                                .password(null)
+                                .scrollHandle(null)
+                                .load();
+
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
-        });
+        }).start();
+//        mView.fromUrl(mUrl, fileName, new PDFView.FileLoadingListener() {
+//            @Override
+//            public void onFileLoaded(File file) {
+//                isLoaded = true;
+//                Log.d("PDFView", "PDF File 加载完成======");
+//                mView.fromFile(file)
+//                        .enableSwipe(true) // allows to block changing pages using swipe
+//                        .defaultPage(0)
+//                        .onError(PdfViewActivity.this)
+//                        // allows to draw something on the current page, usually visible in the middle of the screen
+//                        .onDraw(PdfViewActivity.this)
+//                        // allows to draw something on all pages, separately for every page. Called only for visible pages
+//                        .onLoad(PdfViewActivity.this) // called after document is loaded and starts to be rendered
+//                        .onPageChange(PdfViewActivity.this)
+//                        .swipeHorizontal(false)
+//                        .enableAntialiasing(true)
+//                        .load();
+//            }
+//
+//            @Override
+//            public void onFileLoadFail() {
+//                isLoaded = false;
+//                DialogManager.getInstance().dissMissProgressDialog();
+//            }
+//        });
     }
 
     @Override
