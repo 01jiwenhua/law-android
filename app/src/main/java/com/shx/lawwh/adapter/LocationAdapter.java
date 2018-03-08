7e8b554a19b74d83613a8d159acd9a51e411561a
121880399 <121880399@qq.com>
2018-03-08 09:44:57 +0800

    更改防火间距文案

--- app/src/main/java/com/shx/lawwh/adapter/LocationAdapter.java
+++ app/src/main/java/com/shx/lawwh/adapter/LocationAdapter.java
@@ -1,6 +1,7 @@
 package com.shx.lawwh.adapter;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +12,7 @@ import android.widget.TextView;
 
 import com.shx.lawwh.R;
 import com.shx.lawwh.entity.response.ResponseGasoline;
+import com.shx.lawwh.utils.StringUtil;
 
 import java.util.LinkedList;
 
@@ -21,20 +23,20 @@ import java.util.LinkedList;
 public class LocationAdapter extends BaseAdapter {
     private Context mContext;
     private LinkedList<ResponseGasoline> mList;
-    private boolean hasNext;
+    private boolean isLast;
 
-    public boolean isHasNext() {
-        return hasNext;
+    public boolean isLast() {
+        return isLast;
     }
 
-    public void setHasNext(boolean hasNext) {
-        this.hasNext = hasNext;
+    public void setLast(boolean last) {
+        isLast = last;
     }
 
-    public LocationAdapter(Context mContext, LinkedList<ResponseGasoline> mList, boolean hasNext) {
+    public LocationAdapter(Context mContext, LinkedList<ResponseGasoline> mList, boolean isLast) {
         this.mContext = mContext;
         this.mList = mList;
-        this.hasNext = hasNext;
+        this.isLast = isLast;
     }
 
     @Override
@@ -62,26 +64,37 @@ public class LocationAdapter extends BaseAdapter {
             holder.mValue= (TextView) view.findViewById(R.id.tv_value);
             holder.mIcon= (ImageView) view.findViewById(R.id.iv_icon);
             holder.mNext= (ImageView) view.findViewById(R.id.iv_next);
+            holder.mLayout= (LinearLayout) view.findViewById(R.id.ll_one);
             view.setTag(holder);
         }
         holder= (ViewHolder) view.getTag();
-
-        holder.mKey.setText(mList.get(i).getParent().getName());
         if(i==0){
+            holder.mKey.setText(mList.get(i).getParent().getName());
             holder.mIcon.setVisibility(View.VISIBLE);
+            holder.mLayout.setBackgroundColor(Color.parseColor("#F5F5F5"));
         }else{
             holder.mIcon.setVisibility(View.GONE);
+            holder.mKey.setText(StringUtil.toChinese(String.valueOf(i))+"级选项");
+            holder.mLayout.setBackgroundColor(Color.parseColor("#FFFFFF"));
         }
-        if(mList.get(i).getChild()!=null&&mList.get(i).getChild().size()>0){
-
-                holder.mValue.setText(mList.get(i).getChild().get(0).getName());
-                holder.mNext.setVisibility(View.VISIBLE);
+        if(mList.get(i).getChild()==null||mList.get(i).getChild().size()==0){
+        }else {
+            holder.mValue.setText(mList.get(i).getChild().get(0).getName());
+        }
+        //如果没有下一级
+        if(isLast&&mList.size()==i+1){
+//                holder.mValue.setText(mList.get(i).getChild().get(0).getName());
+//                holder.mNext.setVisibility(View.VISIBLE);
+            holder.mValue.setTextColor(Color.parseColor("#666666"));
         }else{
-            holder.mValue.setText("");
-            holder.mNext.setVisibility(View.GONE);
+//            holder.mValue.setText("");
+//            holder.mNext.setVisibility(View.GONE);
+            holder.mValue.setTextColor(Color.parseColor("#3BA0F3"));
         }
         return view;
     }
+
+
     private class ViewHolder{
         private TextView mKey;
         private TextView mValue;
