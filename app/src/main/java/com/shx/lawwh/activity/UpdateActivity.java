0f02b8fc6c18259c82267b994d67533065e4cd9d
xuan <shx0518@gmail.com>
2019-02-20 22:16:10 +0800

    版本更新

--- app/src/main/java/com/shx/lawwh/activity/UpdateActivity.java
+++ app/src/main/java/com/shx/lawwh/activity/UpdateActivity.java
@@ -1,5 +1,7 @@
 package com.shx.lawwh.activity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
@@ -35,6 +37,14 @@ public class UpdateActivity extends BaseActivity {
         ResponseVersionInfo versionInfo= (ResponseVersionInfo) getIntent().getSerializableExtra("versionInfo");
         mCurrentVersion.setText("版本信息："+ DeviceUtils.getVersionName(    this));
         mNewVersion.setText("版本信息："+versionInfo.getVersionName());
+        findViewById(R.id.update).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("http://60.210.40.196:25018/app-release.apk");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
     }
 
 }
